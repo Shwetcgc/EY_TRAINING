2cc220d24cc5267700659704d4115ac1ee90fefd
Shwet singh <87944018+Shwetcgc@users.noreply.github.com>
2023-12-20 00:24:27 +0530

    loading-files
    all-the-loading-in-sql-database

---
+++ SALES_BR_4382.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+server = 'XWHGQ9CY3\\SQLEXPRESS'
+database = 'sampleproject'
+driver = 'ODBC+Driver+17+for+SQL+Server'
+trusted_connection = 'Yes'
+engine = create_engine(f"mssql+pyodbc://{server}/{database}?driver={driver}&Trusted_Connection={trusted_connection}")
+file_path = r'C:\Users\BD781CZ\Downloads\Source Files\CDW_SAPP_F_SALES_BR_4382.txt'
+data = pd.read_csv(file_path)
+table_name = 'SALES_BR_4382_RETAIL_PROJECT'
+data.to_sql(table_name, engine, if_exists='replace', index=False)
+engine.dispose()
